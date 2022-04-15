e78f028fa6ad2248ab3d583bd4b79a518ec99d90
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-15 03:06:38 -0500

    Added edit and delete post

--- react-front-end/src/components/Dashboard.js
+++ react-front-end/src/components/Dashboard.js
@@ -1,21 +1,29 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Posts from "../components/Posts";
 import PostDetails from "./PostDetails";
 
-class Dashboard extends Component {
-  render() {
-    return (
-      <div className="dashboard">
-        <h2>hellow again</h2>
-        <Posts />
-        <form action="">
-          <input type="text" />
-          <input type="submit" value="Change stuff" />
-        </form>
-        <PostDetails />
-      </div>
-    );
-  }
-}
+export default function Dashboard() {
+  let [posts, setPosts] = useState([
+    { id: 123, title: "banana one", author: "wuff" },
+    { id: 321, title: "fruitt one", author: "meaw" },
+    { id: 534, title: "health one", author: "ding" },
+    { id: 674, title: "inject one", author: "ding" },
+  ]);
 
-export default Dashboard;
+  const [selectedPost, setSelectedPost] = useState(-1);
+
+  return (
+    <div className="dashboard">
+      <h2>Dashboard</h2>
+      <div className="posts">
+        <Posts posts={posts} setSelectedPost={setSelectedPost} />
+      </div>
+      {selectedPost >= 0 && (
+        <PostDetails
+          post={posts.filter((each) => each.id === selectedPost)}
+          updatePost={setPosts}
+        />
+      )}
+    </div>
+  );
+}
--- react-front-end/src/components/Posts.js
+++ react-front-end/src/components/Posts.js
@@ -1,19 +1,9 @@
 import React, { useState, Component } from "react";
 import Post from "./Post";
 
-export default function Posts() {
-  let posts = [
-    { id: 123, title: "banana one", author: "wuff" },
-    { id: 321, title: "fruitt one", author: "meaw" },
-    { id: 534, title: "health one", author: "ding" },
-    { id: 674, title: "inject one", author: "ding" },
-  ];
-
-  let [selectedPost, setSelectedPost] = useState("");
-
+export default function Posts({ posts, setSelectedPost }) {
   const updateSelectedPost = (id) => {
     if (id != null) setSelectedPost(id);
-    console.log("updating ", selectedPost);
   };
 
   return (
